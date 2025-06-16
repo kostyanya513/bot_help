fee5bdd65e700d6fd458acea57695918329902ed
kostyanya513 <126543195+kostyanya513@users.noreply.github.com>
2025-06-16 23:35:42 +0300

    done 1606: added a docstring to the main menu

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,14 +1,22 @@
+"""
+Модуль main_menu содержит клавиатуру главного меню для Telegram-бота.
+"""
 from aiogram import Bot
 from aiogram.types import BotCommand
+from typing import List
 
 from lexicon.lexicon_ru import LEXICON_COMMANDS
 
 
-# Функция для настройки кнопки Menu бота
-async def set_main_menu(bot: Bot):
-    main_menu_commands = [BotCommand(
-        command=command,
-        description=description
-    ) for command,
-        description in LEXICON_COMMANDS.items()]
+async def set_main_menu(bot: Bot) -> None:
+    """
+    Настраивает главное меню бота, устанавливая команды из LEXICON_COMMANDS.
+
+    Args:
+        bot (Bot): Экземпляр бота aiogram.
+    """
+    main_menu_commands: List[BotCommand] = [
+        BotCommand(command=command, description=description)
+        for command, description in LEXICON_COMMANDS.items()
+    ]
     await bot.set_my_commands(main_menu_commands)
